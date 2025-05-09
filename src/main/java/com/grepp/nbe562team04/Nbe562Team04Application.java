fa6670a5af4476dc694990d2a97d0f960eb002e5
moonbong <[GitHub>
2025-05-09 16:48:25 +0900

    create ccm branch

--- src/main/java/com/grepp/nbe562team04/Nbe562Team04Application.java
+++ src/main/java/com/grepp/nbe562team04/Nbe562Team04Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Nbe562Team04Application {
 
+    // hello
     public static void main(String[] args) {
         SpringApplication.run(Nbe562Team04Application.class, args);
     }
