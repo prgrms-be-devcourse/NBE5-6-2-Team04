df5d06d48f6c26cb8cfb21388df824d9082217d0
imseohyeon <forestlight749@gmail.com>
2025-05-12 15:54:42 +0900

    merge (0512): 충돌 해결 > merge

--- src/main/resources/init/data.sql
+++ src/main/resources/init/data.sql
@@ -4,3 +4,4 @@ INSERT INTO level (level_name, level_image_url, xp) VALUES
             ('잎새 개발자', NULL, 400),
             ('나무 개발자', NULL, 700),
             ('숲 개발자', NULL, 1000);
+SELECT * FROM User WHERE user_id = 1;
