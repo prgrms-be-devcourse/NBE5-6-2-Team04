c67cf093949d0c40ab893229304d719edc32fc1e
imseohyeon <forestlight749@gmail.com>
2025-05-13 15:16:11 +0900

    feat (0513): 기본 로그인 (유효성 검사 및 보안 적용 X)

--- src/main/java/com/grepp/nbe562team04/infra/config/SecurityConfig.java
+++ src/main/java/com/grepp/nbe562team04/infra/config/SecurityConfig.java
@@ -1,23 +1,61 @@
 package com.grepp.nbe562team04.infra.config;
 
+import com.grepp.nbe562team04.model.user.entity.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+//    @Bean
+//    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+//        return http.getSharedObject(AuthenticationManagerBuilder.class)
+//            .build();
+//    }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public AuthenticationSuccessHandler successHandler(){
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request,
+                HttpServletResponse response, Authentication authentication)
+                throws IOException, ServletException {
+
+                // 로그인한 사용자 정보 얻기 위한 코드 (삭제 예정)
+                Object principal = authentication.getPrincipal();
+
+                    log.info("로그인 성공: 사용자: {}", principal);
+
+                    boolean isAdmin = authentication.getAuthorities()
+                        .stream()
+                        .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+
+                    if (isAdmin) {
+                        response.sendRedirect("/admin");
+                        return;
+                    }
+
+                    response.sendRedirect("/");
+            }
+        };
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,7 +64,19 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests((authz) -> authz
                 .anyRequest().permitAll()  // 모든 요청 허용
-            );
+            )
+            .formLogin((form) -> form
+                .loginPage("/user/signin")
+                .usernameParameter("email")
+                .loginProcessingUrl("/user/signin")
+                .defaultSuccessUrl("/dashboard, true")
+                .successHandler(successHandler())
+                .permitAll());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
