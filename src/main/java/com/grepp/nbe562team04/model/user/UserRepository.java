1cc898ef9594f70b5a93e7c9de5e25f6261f4b27
imseohyeon <forestlight749@gmail.com>
2025-05-13 14:10:41 +0900

    feat (0513): 기본 회원가입 (유효성 검사 및 보안 적용 X)

--- src/main/java/com/grepp/nbe562team04/model/user/UserRepository.java
+++ src/main/java/com/grepp/nbe562team04/model/user/UserRepository.java
@@ -1,8 +1,9 @@
-package com.grepp.nbe562team04.model.user.repository;
+package com.grepp.nbe562team04.model.user;
 
 import com.grepp.nbe562team04.model.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 기본적인 CRUD 다 제공됨
 }
