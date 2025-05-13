e380dfa1685a65ea66212ddec8901a1c1fccefd1
imseohyeon <forestlight749@gmail.com>
2025-05-13 15:57:03 +0900

    chore (0513): level 초기 데이터 추가

--- src/main/resources/init/data.sql
+++ src/main/resources/init/data.sql
@@ -1,1 +1,6 @@
-SELECT * FROM User WHERE user_id = 1;
+INSERT INTO level (level_name, level_image_url, xp) VALUES
+            ('씨앗 개발자', NULL, 100),
+            ('새싹 개발자', NULL, 200),
+            ('잎새 개발자', NULL, 400),
+            ('나무 개발자', NULL, 700),
+            ('숲 개발자', NULL, 1000);
