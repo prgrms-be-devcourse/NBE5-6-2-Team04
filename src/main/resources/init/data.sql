73b2babfb17ea1ddb5a71eaa66d40a334d63561d
nathan <nathan960307@gmail.com>
2025-05-14 16:41:41 +0900

    fix: (0514/1640) 레벨 db 수정

--- src/main/resources/init/data.sql
+++ src/main/resources/init/data.sql
@@ -1,6 +1,25 @@
 INSERT INTO level (level_name, xp) VALUES
-            ('씨앗 개발자', 100),
+            ('씨앗 개발자', 500),
             ('새싹 개발자', 200),
             ('잎새 개발자', 400),
             ('나무 개발자', 700),
-            ('숲 개발자', 1000);
+            ('숲 개발자', 0);
+
+
+
+
+UPDATE level SET level_name = '씨앗 개발자', xp = 100 WHERE level_id = 1;
+UPDATE level SET level_name = '새싹 개발자', xp = 200 WHERE level_id = 2;
+UPDATE level SET level_name = '잎새 개발자', xp = 400 WHERE level_id = 3;
+UPDATE level SET level_name = '나무 개발자', xp = 700 WHERE level_id = 4;
+UPDATE level SET level_name = '숲 개발자', xp = 1000 WHERE level_id = 5;
+UPDATE level SET level_name = '씨앗 개발자', xp = 155 WHERE level_id = 6;
+UPDATE level SET level_name = '새싹 개발자', xp = 200 WHERE level_id = 7;
+UPDATE level SET level_name = '잎새 개발자', xp = 400 WHERE level_id = 8;
+UPDATE level SET level_name = '나무 개발자', xp = 700 WHERE level_id = 9;
+UPDATE level SET level_name = '숲 개발자', xp = 1000 WHERE level_id = 10;
+UPDATE level SET level_name = '씨앗 개발자', xp = 500 WHERE level_id = 11;
+UPDATE level SET level_name = '새싹 개발자', xp = 200 WHERE level_id = 12;
+UPDATE level SET level_name = '잎새 개발자', xp = 400 WHERE level_id = 13;
+UPDATE level SET level_name = '나무 개발자', xp = 700 WHERE level_id = 14;
+UPDATE level SET level_name = '숲 개발자', xp = 0 WHERE level_id = 15;
