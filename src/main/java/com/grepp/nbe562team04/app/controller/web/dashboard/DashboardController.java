0fd32840297aec8b851fe8b3dba405eb91d8dc5b
0802222 <0802222@naver.com>
2025-05-14 10:41:04 +0900

    chore : develop merge를 위한 사전작업(변경사항 커밋)

--- src/main/java/com/grepp/nbe562team04/app/controller/web/dashboard/DashboardController.java
+++ src/main/java/com/grepp/nbe562team04/app/controller/web/dashboard/DashboardController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Controller
 public class DashboardController {
